6907c9051a32692d4d1be50d7f927d1e21bb30a3
Inez Yulia Amanda <inez.amanda16@gmail.com>
2020-11-11 19:12:03 +0800

    first

---
+++ app/Http/Middleware/OnlyOrdinaryUser.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class OnlyOrdinaryUser
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        //dd($request->user()->role);
+        if ($request->user()->role != 'bukan_admin'){
+            return redirect()->route('notfound');
+        }
+
+        return $next($request);
+    }
+}
